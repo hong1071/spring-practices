e79fe4ca5b9acdd00487a010b995523bdb5ad7cf
changmok <hong1071@nate.com>
2021-10-27 14:22:40 +0900

    explicit-config & jUnit

--- container/src/main/java/com/douzone/container/config/soundsystem/CDPlayerConfig.java
+++ container/src/main/java/com/douzone/container/config/soundsystem/CDPlayerConfig.java
@@ -3,6 +3,12 @@ package com.douzone.container.config.soundsystem;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/*
+ * 
+ * Auto(자동) Configuration
+ * 
+ * */
+
 @Configuration
 @ComponentScan(basePackages = {"com.douzone.container.soundsystem"})	//스캔할 컴포넌트들이 있는 위치
 public class CDPlayerConfig {
