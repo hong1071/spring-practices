c07b5c1aff2762e52d7eedcc6d39b28920d70d66
changmok <hong1071@nate.com>
2021-10-27 11:55:24 +0900

    auto-java config & jUnit

---
+++ container/src/main/java/com/douzone/container/config/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,11 @@
+package com.douzone.container.config.soundsystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.douzone.container.soundsystem"})	//스캔할 컴포넌트들이 있는 위치
+public class CDPlayerConfig {
+
+	
+}
---
+++ container/src/test/java/com/douzone/container/config/soundsystem/CDPlayerJavaConfigTest.java
@@ -0,0 +1,55 @@
+package com.douzone.container.config.soundsystem;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.douzone.container.soundsystem.CDPlayer;
+import com.douzone.container.soundsystem.CompactDisc;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {CDPlayerConfig.class})  //resource가 아닌 java에 있다.
+public class CDPlayerJavaConfigTest {
+
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+	
+	@Autowired
+	private CDPlayer cdplayer;
+	
+	@Autowired
+	private CompactDisc cd;
+	
+	@Test
+	public void testHello() {
+		
+		int a = 10 - 9;
+		assertEquals(1, a);
+	}
+	
+	@Test
+	public void testCDPlayerNotNull() {
+		
+		assertNotNull(cdplayer);
+	}
+	
+	@Test
+	public void testCompactDiscNotNull() {
+		
+		assertNotNull(cd);
+	}
+	
+	@Test
+	public void testPlay() {
+		
+		cdplayer.play();
+		assertEquals("Playing 붕붕 by 김하온", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	}
+}
